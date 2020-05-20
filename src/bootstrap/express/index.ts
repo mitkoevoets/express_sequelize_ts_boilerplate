4ead5726ebb870fcfa74dd1f52d6e3ecbf0b68ce
mitkoevoets <mitkoevoets@vaerens.com>
2020-05-20 20:13:44 +0200

    Adjustments

--- src/bootstrap/express/index.ts
+++ src/bootstrap/express/index.ts
@@ -1,44 +1,25 @@
 import express from 'express';
 import router from '../../routes/index.route';
-import config from '../../config';
 import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 export const app = express();
 
-// secure apps by setting various HTTP headers
+
 /**
  *
+ * Middlewares
  */
 app.use(helmet());
-
-// enable CORS - Cross Origin Resource Sharing
 app.use(cors());
-
-// enable detailed API logging in dev env
-if (config.env === 'development') {
-  expressWinston.requestWhitelist.push('body');
-  expressWinston.responseWhitelist.push('body');
-  app.use(expressWinston.logger({
-    winstonInstance,
-    meta: true, // optional: log meta data about request (defaults to true)
-    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
-    colorStatus: true, // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
-  }));
-}
+app.use(bodyParser.json({limit: '5mb'}));
 
 /**
  *
  * Register routes.
  */
-app.use('/', routes);
-
-/**
- *
- * Error handling.
- */
-app.use(APIErrorHandler);
-app.use(fourOFourHandler);
-app.use(stackTraceHandler);
+app.use('/', router);
 
 /**
  *
@@ -47,4 +28,47 @@ app.use(stackTraceHandler);
 export default app;
 
 
-app.use('/', router);
+//
+// // enable detailed API logging in dev env
+// if (config.env === 'development') {
+//   expressWinston.requestWhitelist.push('body');
+//   expressWinston.responseWhitelist.push('body');
+//   app.use(expressWinston.logger({
+//     winstonInstance,
+//     meta: true, // optional: log meta data about request (defaults to true)
+//     msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+//     colorStatus: true, // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+//   }));
+// }
+//
+// /**
+//  *
+//  * Register routes.
+//  */
+// app.use('/', routes);
+//
+// /**
+//  *
+//  * Error handling.
+//  */
+// app.use(APIErrorHandler);
+// app.use(fourOFourHandler);
+// app.use(stackTraceHandler);
+//
+// /**
+//  *
+//  * Export express app.
+//  */
+// export default app;
+//
+//
+// app.use('/', router);
+//
+// import express from 'express';
+// import router from '../routes/index.route';
+// import helmet from 'helmet';
+// import bodyParser from 'body-parser';
+//
+// export const app = express();
+
+//
